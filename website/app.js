d2167b8f815d51c4d2bef836dfd713fae717f0f7
EsraaMoataz <36828152+EsraaMoataz@users.noreply.github.com>
2020-12-13 23:50:10 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -14,17 +14,19 @@ function generate()
 
     const zipCode=document.getElementById('zip').value;
     const feeling=document.getElementById('feelings').value;
-    getData(baseURL,zipCode,apiKey).then(function(data){
+    if(zipCode)
+    {getData(baseURL,zipCode,apiKey).then(function(data){
         
         postData('/all',{Temp:data.main.temp ,date:newDate,feeling:feeling});
-    })
-    .then(updateUI());
+    }).then(updateUI);
+}
+    else
+    alert('please enter the zip code first');
 }
 
 //GET data from API
 const getData=async(baseURL,zipCode,apiKey)=>{
-    const response=await fetch(baseURL+zipCode+apiKey);
-
+    const response=await fetch(baseURL+zipCode+apiKey)
     try {
         const data=await response.json();
         return data;
@@ -35,7 +37,6 @@ const getData=async(baseURL,zipCode,apiKey)=>{
 
 //POST data to server
 const postData=async(url='',data={})=>{
-    console.log("post data");
     const res= await fetch(url, {
         method: 'POST', 
         credentials: 'same-origin',
@@ -45,13 +46,9 @@ const postData=async(url='',data={})=>{
        // Body data type must match "Content-Type" header        
         body: JSON.stringify(data), 
       });
-      console.log("post data after");
-        console.log(res);
-        
-        try {
+
+      try {
             const newData = await res.json();
-            
-            console.log(newData);
             return ;
         } catch (error) {
             console.log(error);
@@ -61,11 +58,10 @@ const postData=async(url='',data={})=>{
 
 //Update UI
 const updateUI=async()=>{
-    const request=await fetch('/allData');
+    const request=await fetch('/all');
     try {
         
         const dataUpdated=await request.json();
-        console.log(dataUpdated);
         document.getElementById('temp').innerHTML=dataUpdated.Temperature;
         document.getElementById('date').innerHTML=dataUpdated.Date;
         document.getElementById('content').innerHTML=dataUpdated.feelings;
